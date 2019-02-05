3369da601f7678a6eeae48ce7bfd9ee6fa642cb2
mc-greq <greq19@gmail.com>
2019-02-05 21:01:31 +0100

    Deleting course without deleting the instructor (NO cascade)

---
+++ src/example/nio/DeleteCourseDemo.java
@@ -0,0 +1,38 @@
+package example.nio;
+
+import example.nio.entities.Course;
+import example.nio.entities.Instructor;
+import example.nio.entities.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteCourseDemo {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try(factory){
+            session.beginTransaction();
+
+            // get a course
+            int id = 10;
+            Course tempCourse = session.get(Course.class, id);
+
+            // delete course
+            System.out.println("Deleting course: " + tempCourse);
+            session.delete(tempCourse);
+
+            session.getTransaction().commit();
+        }
+    }
+}
